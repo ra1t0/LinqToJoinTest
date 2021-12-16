ec1db88180e6e27c45dc35b041b4164e344bf5af
rikuy <game3280@gmail.com>
2021-12-16 21:51:11 +0900

    Join検証用のクラスを追加しました。

---
+++ LinqToJoinTest/LinqToJoinTest/JoinClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToJoinTest
+{
+    /// <summary>
+    /// 会社名テーブル
+    /// </summary>
+    public class CompanyNameTable
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+    }
+
+    /// <summary>
+    /// ユーザーテーブル
+    /// </summary>
+    public class UserTable
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string CompanyId { get; set; }
+    }
+}
