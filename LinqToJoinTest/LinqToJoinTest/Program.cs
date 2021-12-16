bf57246890497c8003d0552b4fde2778e0b56869
rikuy <game3280@gmail.com>
2021-12-16 22:28:23 +0900

    JoinのLinqを追加しました。使い方が分かった。

--- LinqToJoinTest/LinqToJoinTest/Program.cs
+++ LinqToJoinTest/LinqToJoinTest/Program.cs
@@ -29,3 +29,17 @@ userClass.Add(new JoinClass.UserTable
 });
 
 //Linqを実施してみる
+var ret = companyClass.Join(
+                   userClass,//ここで結合するテーブルを選択する。
+                   companycls => companycls.Id,//ここで結合元テーブルの結合データを選択する。
+                   usercls => usercls.CompanyId,//ここで結合先テーブルの結合データを選択する。
+                   (company, user) => new//ここで欲しい情報を選択して新しいクラスを生成する。
+                   {
+                       CompanyName = company.CompanyName,
+                       UserName = user.UserName
+                   });
+//結果表示
+foreach(var data in ret)
+{
+    Console.WriteLine($"{data.CompanyName}の{data.UserName}");
+}
